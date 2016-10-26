b0f0434ae049bcc5bc9d520963a7fcd5c6795a3a
Mingjian Wen <wenxx151@umn.edu>
2016-10-26 13:17:48 -0500

    Changed Ridder's algorithm to compute finite diff for each component of a vector function

--- openkim_fit/fisher_ridder.py
+++ openkim_fit/fisher_ridder.py
@@ -78,21 +78,22 @@ class Fisher():
 
 def dfridr(func, x, h, *args):
     """
-    Compute numerical derivative using Ridders' method.
+    Compute numerical derivative using Ridders' method of polynomial extrapolation.
 
     In Ridders method, 'func' is a scalar function that takes a scalar argument
-    and return s a scalar. Here, we entend it such that 'func' takes a scalar
-    argument but can return a vector.
-
+    and return s a scalar. Here, we extend it such that 'func' takes a scalar
+    argument but can return a vector. But the derivative of each component is computed
+    separatly.
 
     Parameters
     ----------
     func: function
-        The function whose first derivate we want to estimate. 'func' should
-        should take a scalar argument, and can return either scalar or vector.
+        The function whose first derivate(s) we want to estimate. 'func' should
+        take a scalar argument 'x' and additional arguments 'args', and can return
+        either scalar or vector.
 
     x: float
-        The point at which we want to estimate the derivative.
+        The point at which we want to estimate the derivative(s).
 
     h: float
         initial stepsize
@@ -102,51 +103,52 @@ def dfridr(func, x, h, *args):
     ans: float or list of floats
         the derivative(s)
 
-    err: float
-        error of the finite difference estimation
+    err: float or list of floats
+        error(s) of the finite difference estimation
 
     Reference
     ---------
 
     Based on code from Numerical Recipes, Press et al., Second Ed., Cambridge,
     1992.
-
-    Ref: Ridders, C. J. F., 'Two algorithms for the calculation of dF(x)=D',
-         Advances in Engineering Software, Vol. 4, no. 2, pp. 75-76, 1982.
     """
 
     ntab = 10
-    con = 1.4
-    con2 = con**2
+    con = 1.4; con2 = con**2
     big = np.inf
     safe = 2.0
     size = len(func(x, *args))
     a = np.zeros((ntab, ntab, size))
-
-    if h <= 0.:
-        raise ValueError('h must be larger than 0.0 in dfridr.')
+    if h == 0.:
+        raise ValueError('h must be nonzero in dfridr.')
     hh = h
     a[0][0] = (func(x+hh, *args) - func(x-hh, *args))/(2.*hh)
-    err = big
+    err = [big for i in range(size)]
+    ans = [None for i in range(size)]
+    stop_update = [False for i in range(size)]
+    # Successive columns in the Neville tableau will go to smaller stepsizes and higher
+    # orders of extrapolation.
     for i in range(1, ntab):
-        hh /= con  #try new, smaller step size
+        hh /= con  # try new, smaller step size
         a[0][i] = (func(x+hh, *args) - func(x-hh, *args))/(2.*hh)
         fac = con2
+        # Compute extrapolations of various orders, requiring no new function evaluation.
         for j in range(1,i+1):
             a[j][i] = (a[j-1][i]*fac - a[j-1][i-1])/(fac-1.)
             fac *= con2
             # The error strategy is to compare each new extrapolation to one order lower,
             # both at the present stepsize and the previous one.
-            # this is different from the original Ridder algorithm, where 'a' is scalar
-            # but here we assume 'a' is a 1D array, so we use norm
-#            errt = max(np.linalg.norm(a[j][i] - a[j-1][i]), np.linalg.norm(a[j][i] - a[j-1][i-1]))
-            errt = max(max(abs((a[j][i] - a[j-1][i]))), max(abs((a[j][i] - a[j-1][i-1]))))
-            if errt < err:
-                err = errt
-                ans = a[j][i]
-        #If higher order is worse by a significant factor SAFE, then quit early.
-#        if np.linalg.norm(a[i][i] - a[i-1][i-1]) > safe*err:
-        if max(abs(a[i][i] - a[i-1][i-1])) > safe*err:
+            # note, np.maximum is element-wise
+            errt = np.maximum(np.absolute(a[j][i]-a[j-1][i]), np.absolute(a[j][i]-a[j-1][i-1]))
+            for k in range(size):
+                if not stop_update[k] and errt[k] < err[k]:
+                    err[k] = errt[k]
+                    ans[k] = a[j][i][k]
+        #If higher order is worse by a significant factor SAFE, then stop update.
+        for k in range(size):
+            if not stop_update[k] and abs(a[i][i][k] - a[i-1][i-1][k]) >= safe*err[k]:
+                stop_update[k] = True
+        if all(stop_update):
             break
     return ans,err
 
@@ -154,42 +156,26 @@ def dfridr(func, x, h, *args):
 
 if __name__ == '__main__':
 
-#
-#    # test dfridr
-#    def func(x):
-#        return np.array((np.sin(x), np.cos(x), x**2))
-#    def func_grad(x):
-#        return np.array((np.cos(x), -np.sin(x), 2*x))
-#    def simple_finite_diff(func, x, stepsize):
-#        return (func(x+stepsize)-func(x-stepsize))/(2.*stepsize)
-#    x = np.pi/4 #    deri,err = dfridr(func, x, 0.1)
-#    analytic=func_grad(x)
-#    simple_fi = simple_finite_diff(func, x, 1e-8)
-#
-#    print 'finite diff:'
-#    for i,j,k in zip(deri,analytic, simple_fi):
-#        print '{:24.16e}   {:24.16e} {:24.16e}'.format(i,j,k)
-#
-#    sys.exit(1)
-#
-
-
-
     from training import TrainingSet
     from modelparams import ModelParams
     from kimcalculator import KIMcalculator
+
+    # Temperature
+    T = 450
+
 
     # KIM model parameters
     modelname = 'Three_Body_Stillinger_Weber_MoS__MO_000000111111_000'
     params = ModelParams(modelname)
     params.echo_avail_params()
-    fname = '../tests/mos2_fitted.txt'
+    fname = '../tests/mos2_fitted-T'+str(T)
     params.read(fname)
     params.echo_params()
 
     # read config and reference data
     tset = TrainingSet()
-    tset.read('../tests/training_set-T300')
+    fname = '../tests/training_set-T'+str(T)
+    tset.read(fname)
     configs = tset.get_configs()
 
     # prediction
@@ -202,51 +188,80 @@ if __name__ == '__main__':
     print 'hello there, started computing Fisher information matrix. It may take a'
     print 'while, so take a cup of coffee.'
     fisher = Fisher(KIMobjs, params)
-    fisher_matrix = fisher.compute_fisher_matrix()
+    Fij = fisher.compute_fisher_matrix()
+
+    # pickle
+#    fname = 'fisher_mat'
+#    np.save(fname, Fij)
+
+#    # load
+#    fname = 'fisher_mat.npy'
+#    Fij = np.load(fname)
 
 
-    # Temperature
-    T = 150
     kB = 8.61733034e-5
     gamma = 1
-    fisher_matrix = fisher_matrix/(2.*kB*T*gamma)
+    Fij = Fij/(2.*kB*T*gamma)
 
+    # relative variance of Fij
+    param_values = params.get_x0()
 
-    with open('Fij', 'w') as fout:
-        for line in fisher_matrix:
-            for i in line:
-                fout.write('{:24.16e} '.format(i))
+    for t in range(1):
+
+        Fij_rv = np.dot(np.dot(np.diag(param_values), Fij), np.diag(param_values))
+
+        with open('Fij'+str(t), 'w') as fout:
+            for line in Fij_rv:
+                for i in line:
+                    fout.write('{:24.16e} '.format(i))
+                    #fout.write('{:10.2e} '.format(i))
+                fout.write('\n')
+
+        Fij_diag = np.diag(Fij_rv)
+        with open('Fij_diag'+str(t), 'w') as fout:
+            for line in Fij_diag:
+                #fout.write('{:13.5e}\n'.format(line))
+                fout.write('{:13.5e}\n'.format(line**0.5))
+
+        # inverse
+        Fij_inv = np.linalg.inv(Fij_rv)
+        with open('Fij_inv'+str(t), 'w') as fout:
+            for line in Fij_inv:
+                for i in line:
+                    fout.write('{:24.16e} '.format(i))
+                    #fout.write('{:10.2e} '.format(i))
+                fout.write('\n')
+
+        # inverse_diag
+        Fij_inv_diag = np.diag(Fij_inv)
+        with open('Fij_inv_diag'+str(t), 'w') as fout:
+            for line in Fij_inv_diag:
+                #fout.write('{:13.5e}\n'.format(line))
+                fout.write('{:13.5e}\n'.format(line**0.5))
+
+        # eiven analysis
+        w,v = np.linalg.eig(Fij_rv)
+        with open('eigenVec'+str(t),'w') as fout:
+          for row in v:
+            for item in row:
+              fout.write('{:13.5e}'.format(float(item)))
             fout.write('\n')
 
-    Fij_diag = np.diag(fisher_matrix)
-    with open('Fij_diag', 'w') as fout:
-        for line in Fij_diag:
-            fout.write('{:13.5e}\n'.format(line))
-
-    # inverse
-    Fij_inv = np.linalg.inv(fisher_matrix)
-    with open('Fij_inv', 'w') as fout:
-        for line in Fij_inv:
-            for i in line:
-                fout.write(str(i)+ ' ')
-            fout.write('\n')
-
-    # inverse_diag
-    Fij_inv_diag = np.diag(Fij_inv)
-    with open('Fij_inv_diag', 'w') as fout:
-        for line in Fij_inv_diag:
-            fout.write('{:13.5e}\n'.format(line))
-
-    # eiven analysis
-    w,v = np.linalg.eig(fisher_matrix)
-    with open('eigenVec','w') as fout:
-      for row in v:
-        for item in row:
-          fout.write('{:13.5e}'.format(float(item)))
-        fout.write('\n')
-
-    with open('eigenVal','w') as fout:
-      for item in w:
-        fout.write('{:13.5e}\n'.format(float(item)))
+        with open('eigenVal'+str(t),'w') as fout:
+          for item in w:
+            fout.write('{:13.5e}\n'.format(float(item)))
 
 
+        # update for the next loop
+#        param_values = np.dot(v.T, param_values)
+#        Fij = np.dot( np.dot(np.linalg.inv(v), Fij), np.linalg.inv(v.T))
+
+
+#        # remove the smallest eivenval
+#        v = v[:,:-1]
+#        # update for the next loop
+#        param_values = np.dot(v.T, param_values)
+#        Fij = np.dot( np.dot(np.linalg.pinv(v), Fij), np.linalg.pinv(v.T))
+#
+#
+#
