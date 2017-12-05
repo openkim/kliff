4147f7a25825727b444a1db14c9c47080d00e316
Mingjian Wen <wenxx151@umn.edu>
2017-12-05 17:16:48 -0600

    added automatic determination of layer_cut in layer.cpp

--- openkim_fit/layers.cpp
+++ openkim_fit/layers.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "layers.h"
 
 #define DIM 3
@@ -8,6 +9,12 @@ typedef double vectorOfSizeDIM[DIM];
 
 
 // create layers
+// To assign atoms into different layers. If `ruct_layer < 0', it will be
+// determined internally within the code by finding the max of the min of pair
+// distance between eatch atom and its neighbors. This is a bit more expensive
+// since it runs through the neighborlist once.
+//
+
 int create_layers(const int Natoms, double* coordinates,
     const int* neighlist, const int* numneigh, const double rcut_layer,
     std::vector<int>& in_layer)
@@ -23,9 +30,43 @@ int create_layers(const int Natoms, double* coordinates,
     first_neigh[i] = first_neigh[i-1] + numneigh[i-1];
   }
 
+  // cutoff used to find atoms in the same layer
+
+  double cutsq_layer;
+  if (rcut_layer > 0) {
+    cutsq_layer = rcut_layer * rcut_layer;
+  }
+  else {   // max of min of pair distance is rcut_layer
+
+    std::vector<double> min_rsq(Natoms, 1e10);
+
+    for (int i=0; i<Natoms; i++) {
+      int num_neigh = numneigh[i];
+      const int* ilist = neighlist + first_neigh[i];
+
+      for (int jj=0; jj<num_neigh; ++jj) {
+        int j = ilist[jj];
+
+        // squared distance of rij
+        double delx = coords[j][0]-coords[i][0];
+        double dely = coords[j][1]-coords[i][1];
+        double delz = coords[j][2]-coords[i][2];
+        double rsq = delx*delx + dely*dely + delz*delz;
+
+        if (rsq < min_rsq[i]) {
+          min_rsq[i] = rsq;
+        }
+      }
+    }
+
+    double max_min_rsq = *(std::max_element(min_rsq.begin(), min_rsq.end()));
+    cutsq_layer = max_min_rsq * 1.01;  // *1.01 to get over edge case
+  }
+  // we can set it manually to speed up
+  // cutsq_layer = (0.72*3.35)*(0.72*3.35);
+
+
   // layers
-  //double cutsq_layer = rcut_layer * rcut_layer;
-  double cutsq_layer = (0.72*3.35)*(0.72*3.35);
   int nremain; // number of atoms not included in any layer
   int nlayers;
 
@@ -67,9 +108,9 @@ int create_layers(const int Natoms, double* coordinates,
         int j = ilist[jj];
 
         // squared distance of rij
-        double delx = (coords[j][0]-coords[i][0]) * (coords[j][0]-coords[i][0]);
-        double dely = (coords[j][1]-coords[i][1]) * (coords[j][1]-coords[i][1]);
-        double delz = (coords[j][2]-coords[i][2]) * (coords[j][2]-coords[i][2]);
+        double delx = coords[j][0]-coords[i][0];
+        double dely = coords[j][1]-coords[i][1];
+        double delz = coords[j][2]-coords[i][2];
         double rsq = delx*delx + dely*dely + delz*delz;
 
         // should be included in current layer
@@ -108,6 +149,7 @@ int create_layers(const int Natoms, double* coordinates,
 
 
   //TODO delete debug
+  std::cout<<"Cutoff for layer "<<sqrt(cutsq_layer)<<std::endl;
   std::cout <<"Number of layers: " <<nlayers <<std::endl;
   std::cout <<"#atom id     layer"<<std::endl;
   for (int i=0; i<Natoms; i++) {
