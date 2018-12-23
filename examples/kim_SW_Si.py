1a2412897d0f9afd25ab6a9c9a4ed8048d47af6d
Mingjian Wen <wenxx151@umn.edu>
2018-12-23 02:32:50 -0600

    Update kim_SW_Si.py to set parameter `lambda` (python keyword) using `set_one`

--- examples/kim_SW_Si.py
+++ examples/kim_SW_Si.py
@@ -5,24 +5,30 @@ from kliff.loss import Loss
 
 kliff.logger.set_level('debug')
 
+
 # training set
 tset = DataSet()
 tset.read('../tests/calculators/Si_T300_4/')
 configs = tset.get_configurations()
+
 
 # calculator
 calc = KIM(model_name='Three_Body_Stillinger_Weber_Si__MO_405512056662_004')
 # calc.echo_model_params()
 calc.create(configs)
 
+
 # fitting parameters
 calc.set_fitting_params(
     A=[[16.0, 1., 20]],
     B=[['DEFAULT']],
     sigma=[[2.0951, 'fix']],
-    _lambda=[[45.5322]],
     gamma=[[2.51412]])
+# "lambda" is a python keyword, cannot use the above method
+calc.set_one_fitting_params(name='lambda', settings=[[45.5322]])
+
 # calc.echo_fitting_params()
+
 
 # loss
 with Loss(calc, nprocs=2) as loss:
