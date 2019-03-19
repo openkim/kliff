b9bf249f0450b19ab4e4ea81841d06cf428dd775
Mingjian Wen <wenxx151@umn.edu>
2019-03-19 11:56:15 -0500

    Enable __enter__ for Loss

--- examples/lennard_jones.py
+++ examples/lennard_jones.py
@@ -7,7 +7,7 @@ kliff.logger.set_level('debug')
 
 # training set
 tset = DataSet()
-tset.read('../tests/calculators/Si_T300_4/')
+tset.read('../tests/configs_extxyz/Si_4/')
 configs = tset.get_configurations()
 
 # calculator
