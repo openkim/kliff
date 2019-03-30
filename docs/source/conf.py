822bb59b332d2656ba2a59a4f03ad083bbb78035
Mingjian Wen <wenxx151@umn.edu>
2019-03-29 19:59:22 -0500

    Use pygments_style='default'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 #pygments_style = 'sphinx'
-#pygments_style = 'default'
+pygments_style = 'default'
 #pygments_style = None
 
 
