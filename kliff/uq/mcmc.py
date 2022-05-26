3afd5b30d85e32bb463165180192cbbc4c704c2c
yonatank <kurniawanyo@outlook.com>
2022-05-26 12:09:03 -0600

    Initial commit of the uq implementation
    * For now, we will just implement MCMC via `ptemcee` and `emcee`
    Python packages.

---
+++ kliff/uq/mcmc.py
@@ -0,0 +1,350 @@
+from typing import Any, Callable, Dict, List, Optional
+import copy
+
+import numpy as np
+
+from kliff.loss import Loss
+from kliff.error import report_import_error
+
+try:
+    import emcee
+
+    emcee_avail = True
+except ImportError:
+    emcee_avail = False
+
+try:
+    import ptemcee
+
+    ptemcee_avail = True
+except ImportError:
+    ptemcee_avail = False
+
+
+def logprior_uniform(x: np.ndarray, bounds: np.ndarray) -> float:
+    """Logarithm of the non-normalized joint uniform prior. This is the default prior
+    distribution.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Parameter values to evaluate.
+    bounds : np.ndarray (nparams, 2,)
+        An array containing the boundaries of the uniform prior. The first column of the
+        array contains the lower bounds and the second column contains the upper bounds.
+
+    Returns
+    -------
+    float:
+        Logarithm of the non-normalized joint uniform prior evaluated at parameter ``x``.
+    """
+    l_bounds, u_bounds = bounds.T
+    if all(np.less(x, u_bounds)) and all(np.greater(x, l_bounds)):
+        ret = 0.0
+    else:
+        ret = -np.inf
+
+    return ret
+
+
+def get_T0(loss: Loss):
+    """Compute the natural temperature. The minimum loss is the loss value at the optimal
+    parameters.
+
+    Parameters
+    ----------
+    loss : Loss
+        Loss function class from :class:`~kliff.loss.Loss`.
+
+    Returns
+    -------
+    float
+        Value of the natural temperature.
+    """
+    xopt = loss.calculator.get_opt_params()
+    ndim = len(xopt)
+    C0 = loss._get_loss(xopt)
+    return 2 * C0 / ndim
+
+
+class MCMC:
+    """MCMC sampler class for Bayesian uncertainty quantification.
+
+    This is a wrapper over :class:`PtemceeSampler` and
+    :class:`EmceeSampler`. Currently, there are only these 2 samplers
+    implemented.
+
+    Parameters
+    ----------
+    loss : Loss
+        Loss function class from :class:`~kliff.loss.Loss`.
+    nwalkers : Optional[int]
+        Number of walkers to simulate. The minimum number of walkers
+        is twice the number of parameters. It defaults to this minimum
+        value.
+    logprior_fn : Optional[Callable]
+        A function that evaluate logarithm of the prior
+        distribution. The prior doesn't need to be normalized. It
+        defaults to a uniform prior over a finite range.
+    logprior_args : Optional[tuple]
+        Additional positional arguments of the ``logprior_fn``. If the
+        default ``logprior_fn`` is used, then the boundaries of the
+        uniform prior can be specified here.
+    use_ptsampler : Optional[bool]
+        A flag to use parallel tempered sampler. It defaults to
+        ``True``.
+    **kwargs : Optional[dict]
+        Additional keyword arguments for the sampler. See ``ptemcee.Sampler`` if
+        ``use_ptsampler`` is true, otherwise see ``emcee.EnsembleSampler``.
+    """
+
+    def __new__(
+        self,
+        loss: Loss,
+        nwalkers: Optional[int] = None,
+        logprior_fn: Optional[Callable] = None,
+        logprior_args: Optional[tuple] = (),
+        use_ptsampler: Optional[bool] = True,
+        **kwargs
+    ):
+
+        if use_ptsampler:
+            if ptemcee_avail:
+                ntemps = kwargs.pop("ntemps") if "ntemps" in kwargs else 10
+                Tmax_ratio = kwargs.pop("Tmax_ratio") if "Tmax_ratio" in kwargs else 1.0
+                Tladder = kwargs.pop("Tladder") if "Tladder" in kwargs else None
+
+                return PtemceeSampler(
+                    loss,
+                    nwalkers,
+                    ntemps,
+                    Tmax_ratio,
+                    Tladder,
+                    logprior_fn,
+                    logprior_args,
+                    **kwargs
+                )
+            else:
+                report_import_error("ptemcee")
+        else:
+            if emcee_avail:
+                return EmceeSampler(
+                    loss, nwalkers, logprior_fn, logprior_args, **kwargs
+                )
+
+            else:
+                report_import_error("emcee")
+
+
+class PtemceeSampler:
+    """Sampler class for PTMCMC.
+
+    Parameters
+    ----------
+    loss : Loss
+        Loss function class from :class:`~kliff.loss.Loss`.
+    nwalkers : Optional[int]
+        Number of walkers to simulate. The minimum number of walkers
+        is twice the number of parameters. It defaults to this minimum
+        value.
+    ntemps: Optional[int]
+        Number of temperatures to simulate. It defaults to 10.
+    Tmax_ratio: Optional[float]
+        The ratio between the highest temperature to use and the natural temperature.
+        Higher value means that the maximum temperature is higher than :math:`T_0`. It
+        defaults to 1.0.
+    Tladder: Optional[List]
+        A list containing the temperature values to use. The values nedd to be
+        monotonically increasing or decreasing. It defaults to ``None``, which will be
+        generated from ``ntemps`` and ``Tmax_ratio``.
+    logprior_fn : Optional[Callable]
+        A function that evaluate logarithm of the prior
+        distribution. The prior doesn't need to be normalized. It
+        defaults to a uniform prior over a finite range.
+    logprior_args : Optional[tuple]
+        Additional positional arguments of the ``logprior_fn``. If the
+        default ``logprior_fn`` is used, then the boundaries of the
+        uniform prior can be specified here.
+    **kwargs : Optional[dict]
+        Additional keyword arguments for ``ptemcee.Sampler``.
+    """
+
+    def __init__(
+        self,
+        loss: Loss,
+        nwalkers: Optional[int] = None,
+        ntemps: Optional[int] = 10,
+        Tmax_ratio: Optional[float] = 1.0,
+        Tladder: Optional[List] = None,
+        logprior_fn: Optional[Callable] = None,
+        logprior_args: Optional[tuple] = (),
+        **kwargs
+    ):
+
+        self.loss = loss
+
+        # Dimensionality
+        ndim = loss.calculator.get_num_opt_params()
+        nwalkers = 2 * ndim if nwalkers is None else nwalkers
+
+        # Probability
+        global loglikelihood_fn
+
+        def loglikelihood_fn(x):
+            return _get_loglikelihood(x, loss)
+
+        if logprior_fn is None:
+            logprior_fn = logprior_uniform
+            if not logprior_args:
+                logprior_args = (_get_parameter_bounds(loss),)
+
+        # Sampling temperatures
+        self.T0 = None
+        self.Tladder = self._generate_temp_ladder(ntemps, Tmax_ratio, Tladder)
+        betas = 1 / self.Tladder
+
+        self.sampler = ptemcee.Sampler(
+            nwalkers,
+            ndim,
+            loglikelihood_fn,
+            logprior_fn,
+            logpargs=logprior_args,
+            betas=betas,
+            **kwargs
+        )
+
+    def run_mcmc(self, *args, **kwargs):
+        """Run the MCMC simulation. For the arguments, see ``ptemcee.Sampler.sample``."""
+        self.sampler.run_mcmc(*args, **kwargs)
+
+    @property
+    def chain(self):
+        """Retrieve the chains from the MCMC simulation."""
+        return self.sampler.chain
+
+    def _generate_temp_ladder(self, ntemps, Tmax_ratio, Tladder):
+        """Generate temperature ladder"""
+        # Only generate temperature ladder when it is not specified.
+        if Tladder is None:
+            # Compute T0
+            self.T0 = get_T0(self.loss)
+
+            Tmax = Tmax_ratio * self.T0
+            Tmax_not_T0 = Tmax_ratio != 1.0
+
+            # If Tmax is not T0, then we need to generate 1 less temperature than
+            # requested, and append T0 afterward.
+            if Tmax_not_T0:
+                ntemps -= 1
+
+            Tladder = np.logspace(0, np.log10(Tmax), ntemps)
+
+            if Tmax_not_T0:
+                Tladder = np.sort(np.append(Tladder, self.T0))
+        return Tladder
+
+
+class EmceeSampler:
+    """Sampler class for affine invariant MCMC.
+
+    Parameters
+    ----------
+    loss : Loss
+        Loss function class from :class:`~kliff.loss.Loss`.
+    nwalkers : Optional[int]
+        Number of walkers to simulate. The minimum number of walkers
+        is twice the number of parameters. It defaults to this minimum
+        value.
+    logprior_fn : Optional[Callable]
+        A function that evaluate logarithm of the prior
+        distribution. The prior doesn't need to be normalized. It
+        defaults to a uniform prior over a finite range.
+    logprior_args : Optional[tuple]
+        Additional positional arguments of the ``logprior_fn``. If the
+        default ``logprior_fn`` is used, then the boundaries of the
+        uniform prior can be specified here.
+    **kwargs : Optional[dict]
+        Additional keyword arguments for ``emcee.EnsembleSampler``.
+    """
+
+    def __init__(
+        self,
+        loss: Loss,
+        nwalkers: Optional[int] = None,
+        logprior_fn: Optional[Callable] = None,
+        logprior_args: Optional[tuple] = (),
+        **kwargs
+    ):
+        self.loss = loss
+
+        # Dimensionality
+        ndim = loss.calculator.get_num_opt_params()
+        nwalkers = 2 * ndim if nwalkers is None else nwalkers
+
+        # Probability
+        self.T0 = get_T0(self.loss)
+        logl_fn = self._loglikelihood_wrapper
+        logp_fn = self._logprior_wrapper(logprior_fn, logprior_args)
+
+        global logprobability_fn
+
+        def logprobability_fn(x):
+            return logl_fn(x) + logp_fn(x)
+
+        self.sampler = emcee.EnsembleSampler(
+            nwalkers, ndim, logprobability_fn, **kwargs
+        )
+
+    def run_mcmc(self, *args, **kwargs):
+        """Run the MCMC simulation. For the arguments, see ``ptemcee.Sampler.sample``."""
+        self.sampler.run_mcmc(*args, **kwargs)
+
+    @property
+    def chain(self):
+        """Retrieve the chains from the MCMC simulation."""
+        return self.sampler.chain
+
+    def _loglikelihood_wrapper(self, x):
+        """A wrapper to the log-likelihood function, so that the only argument is the
+        parameter values.
+        """
+        return _get_loglikelihood(x, self.loss, self.T0)
+
+    def _logprior_wrapper(self, logprior_fn, logprior_args):
+        """A wapper to the log-prior function, so that the only argument is the parameter
+        values.
+        """
+        if logprior_fn is None:
+            if not logprior_args:
+                logprior_args = (_get_parameter_bounds(self.loss),)
+            logprior_fn = logprior_uniform
+
+        def logp(x):
+            return logprior_fn(x, *logprior_args)
+
+        return logp
+
+
+def _get_loglikelihood(x: np.ndarray, loss: Loss, T: Optional[float] = 1.0):
+    """Compute the log-likelihood from the cost function. It has an option to temper the
+    cost by specifying ``T``.
+    """
+    cost = loss._get_loss(x)
+    logl = -cost / T
+    return logl
+
+
+def _get_parameter_bounds(loss):
+    """Get the parameter bounds for the default uniform prior."""
+    bounds = loss.calculator.get_opt_params_bounds()
+    if np.any(np.isin(bounds, None)):
+        raise MCMCError(
+            "Improper uniform prior bound is used. Please specify a finite range."
+        )
+    return np.array(bounds)
+
+
+class MCMCError(Exception):
+    def __init__(self, msg):
+        super(MCMCError, self).__init__(msg)
+        self.msg = msg
---
+++ kliff/uq/mcmc_utils.py
@@ -0,0 +1,188 @@
+from typing import Optional
+
+import numpy as np
+import emcee
+
+
+# Estimate the burn-in time
+def mser(
+    chain: np.ndarray,
+    window: Optional[int] = 1,
+    dmin: Optional[int] = 0,
+    dstep: Optional[int] = 10,
+    dmax: Optional[int] = -1,
+    full_output: Optional[bool] = False,
+) -> int:
+    """Estimate the equilibration time using marginal standard error rule
+    (MSER). This is done by calculating the standard error (square) of chain_d,
+    where chain_d contains the last n-d element of the chain, for progresively
+    larger d values, starting from dmin, incremented by dstep. The SE values
+    are stored in a list. Then we search the minimum element in the list and
+    return the index of that element. To speed up the process, window width can
+    be specified. The chain will be redefined to be the mean of the elements in
+    every window.
+
+    Parameters
+    ----------
+    chain: 1D np.ndarray
+        Array containing the time series.
+    window: int
+        Width of the window in the time series.
+    dmin: int
+        Index where to start the search in the time series.
+    dstep: int
+        How much to increment the search is done.
+    dmax: int
+        Index where to stop the search in the time series.
+    full_output: bool
+        A flag to return the list of squared standard error.
+
+    Returns
+    -------
+    dstar: int
+        Estimate of the equilibration time using MSER.
+
+    Notes
+    -----
+    The argument window needs to be accounted when setting ``dmin``, ``dstep``
+    and ``dmax``.
+    """
+    # Apply the argument window
+    chain_reduced, dmax = _apply_window(chain, window, dmin, dmax)
+
+    # Compute the SE square
+    SE2_list = [
+        _standard_error_squared(chain_reduced[dd:])
+        for dd in np.arange(dmin, dmax, dstep)
+    ]
+
+    # Get the estimate of the equilibration time, wrt the original time series
+    dtemp = np.argmin(SE2_list)
+    dstar = dmin + (dtemp + 1) * dstep * window
+
+    if full_output:
+        to_return = {"dstar": dstar, "SE2": SE2_list}
+    else:
+        to_return = dstar
+    return to_return
+
+
+# Estimate autocorrelation length
+def autocorr(chain: np.ndarray, *args, **kwargs):
+    """Use ``emcee`` package to estimate the autocorrelation length.
+
+    Parameters
+    ----------
+    chain: np.ndarray (nsteps, nwalkers, ndim,)
+        Chains from the MCMC simulation. The shape of the chains needs to be
+        (nsteps, nwalkers, ndim). Note that the burn-in time needs to be discarded prior
+        to this calculation
+    args, kwargs
+        Additional positional and keyword arguments of ``emcee.autocorr.integrated_time``.
+
+    Returns
+    -------
+    float or array:
+        Estimate of the autocorrelation length for each parameter.
+    """
+    return emcee.autocorr.integrated_time(chain, *args, **kwargs)
+
+
+# Assess convergence
+def rhat(chain, time_axis: int = 1, return_WB: bool = False):
+    """Compute the value of :math:`\\hat{r}` proposed by Brooks and Gelman
+    (1998). If the samples come from PTMCMC simulation, then the chain needs
+    to be from one of the temperature only.
+
+    Parameters
+    ----------
+    chain: ndarray
+        The MCMC chain as a ndarray, preferrably with the shape
+        (nwalkers, nsteps, ndims). However, the shape can also be
+        (nsteps, nwalkers, ndims), but the argument time_axis needs to be set
+        to 0.
+    time_axis: int (optional)
+        Axis in which the time series is stored (0 or 1). For emcee results,
+        the time series is stored in axis 0, but for ptemcee for a given
+        temperature, the time axis is 1.
+    return_WB: bool (optional)
+        A flag to return covariance matrices within and between chains.
+
+    Returns
+    -------
+    r: float
+        The value of rhat.
+    W, B: 2d ndarray
+        Matrices of covariance within and between the chains.
+    """
+    if time_axis == 1:
+        # Reshape the chain so that the time axis is in axis 1
+        temp = np.swapaxes(chain, 0, 1)
+        chain = temp
+
+    m, n, _ = chain.shape
+    lambda1, W, B = _lambda1(chain)
+    r = 1 - 1 / n + (1 + 1 / m) * lambda1
+
+    if return_WB:
+        toreturn = (r, W, B)
+    else:
+        toreturn = r
+
+    return toreturn
+
+
+def _apply_window(chain, window, dmin, dmax):
+    """Apply the argument ``window``. If ``window`` > 1, then the chain will be averaged
+    after ``window`` steps, thus giving a shorter chain. This function will also deal
+    with the argument ``dmax``, setting it to the proper value.
+    """
+    chain = chain[dmin:]
+    npoints = len(chain)
+    leftover = npoints % window
+    endpoints = None
+
+    if leftover:
+        endpoints = chain[-leftover:]
+        chain = chain[:-leftover]
+
+    # Reshape chains and take average for every window
+    chain_reduced = chain.reshape((-1, window))
+    chain_reduced = np.mean(chain_reduced, axis=1)
+    if leftover:
+        chain_reduced = np.append(chain_reduced, np.mean(endpoints))
+    dmax = np.min([dmax, len(chain_reduced)])
+    if dmax == -1:
+        dmax = len(chain_reduced)
+
+    return chain_reduced, dmax
+
+
+def _standard_error_squared(chain: np.ndarray) -> float:
+    """Compute the square of the standard error."""
+    nn = len(chain)
+    se2 = np.var(chain) / nn
+    return se2
+
+
+def _lambda1(chain):
+    """Compute the largest eigenvalue of :math:`W^{-1} B/n`."""
+    W = _W(chain)
+    B_over_n = _B_over_n(chain)
+    V = np.linalg.lstsq(W, B_over_n, rcond=-1)[0]
+    s = np.linalg.svd(V, compute_uv=False)
+    return np.max(s), W, B_over_n
+
+
+def _B_over_n(chain):
+    """Compute covariance matrix between the chains."""
+    return np.cov(np.mean(chain, axis=1), rowvar=False, ddof=1)
+
+
+def _W(chain):
+    """Compute the mean of the covariance matrix within each chain."""
+    m, n, nparams = chain.shape
+    Wm = np.empty((m, nparams, nparams))
+    for walker in range(m):
+        Wm[walker] = np.cov((chain[walker]), rowvar=False, ddof=1)
+    return np.mean(Wm, axis=0)
