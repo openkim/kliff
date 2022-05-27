4b47bdf30a0176e079c5de77f48aea185e976d92
yonatank <kurniawanyo@outlook.com>
2022-05-27 09:35:32 -0600

    Only run some test when emcee or ptemcee are installed

--- tests/uq/test_mcmc.py
+++ tests/uq/test_mcmc.py
@@ -9,8 +9,6 @@ from kliff.loss import Loss
 from kliff.models import KIMModel
 from kliff.uq import get_T0, MCMC, PtemceeSampler, EmceeSampler
 
-
-# Run a short PTMCMC
 # model
 modelname = "SW_StillingerWeber_1985_Si__MO_405512056662_006"
 model = KIMModel(modelname)
@@ -37,10 +35,21 @@ nsteps = np.random.randint(1, 5)
 
 # samplers
 prior_bounds = np.tile([0, 10], (ndim, 1))
-ptsampler = MCMC(loss, ntemps=ntemps, nwalkers=nwalkers, logprior_args=(prior_bounds,))
-sampler = MCMC(
-    loss, nwalkers=nwalkers, logprior_args=(prior_bounds,), use_ptsampler=False
-)
+ptemcee_avail = True
+emcee_avail = True
+try:
+    ptsampler = MCMC(
+        loss, ntemps=ntemps, nwalkers=nwalkers, logprior_args=(prior_bounds,)
+    )
+except ImportError:
+    ptemcee_avail = False
+
+try:
+    sampler = MCMC(
+        loss, nwalkers=nwalkers, logprior_args=(prior_bounds,), use_ptsampler=False
+    )
+except ImportError:
+    emcee_avail = False
 
 
 def test_T0():
@@ -57,10 +66,14 @@ def test_T0():
 
 def test_MCMC_wrapper():
     """Test if the MCMC wrapper class returns the correct sampler instance."""
-    assert (
-        type(ptsampler) == PtemceeSampler
-    ), "MCMC should return ``PtemceeSampler`` instance"
-    assert type(sampler) == EmceeSampler, "MCMC should return ``EmceeSampler`` instance"
+    if ptemcee_avail:
+        assert (
+            type(ptsampler) == PtemceeSampler
+        ), "MCMC should return ``PtemceeSampler`` instance"
+    if emcee_avail:
+        assert (
+            type(sampler) == EmceeSampler
+        ), "MCMC should return ``EmceeSampler`` instance"
 
 
 def test_dimensionality():
@@ -70,23 +83,25 @@ def test_dimensionality():
     """
 
     # Test for ptemcee wrapper
-    p0 = np.random.uniform(0, 10, (ntemps, nwalkers, ndim))
-    ptsampler.run_mcmc(p0=p0, iterations=nsteps)
-    assert ptsampler.chain.shape == (
-        ntemps,
-        nwalkers,
-        nsteps,
-        ndim,
-    ), "Dimensionality from the ptemcee wrapper is not right"
+    if ptemcee_avail:
+        p0 = np.random.uniform(0, 10, (ntemps, nwalkers, ndim))
+        ptsampler.run_mcmc(p0=p0, iterations=nsteps)
+        assert ptsampler.chain.shape == (
+            ntemps,
+            nwalkers,
+            nsteps,
+            ndim,
+        ), "Dimensionality from the ptemcee wrapper is not right"
 
     # Test for emcee wrapper
-    p0 = np.random.uniform(0, 10, (nwalkers, ndim))
-    sampler.run_mcmc(initial_state=p0, nsteps=nsteps)
-    assert sampler.chain.shape == (
-        nwalkers,
-        nsteps,
-        ndim,
-    ), "Dimensionality from the emcee wrapper is not right"
+    if emcee_avail:
+        p0 = np.random.uniform(0, 10, (nwalkers, ndim))
+        sampler.run_mcmc(initial_state=p0, nsteps=nsteps)
+        assert sampler.chain.shape == (
+            nwalkers,
+            nsteps,
+            ndim,
+        ), "Dimensionality from the emcee wrapper is not right"
 
 
 if __name__ == "__main__":
