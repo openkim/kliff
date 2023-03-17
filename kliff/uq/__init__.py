44c26d9115aa804ac72362ac6283b13b7efa5f7c
Yonatan Kurniawan <kurniawanyo@outlook.com>
2023-03-17 11:43:38 -0600

    Refactoring
    * Create a parent class for the 2 bootstrap classes, because they have
      many same methods.
    * Create a wrapper class to automatically pick between the 2 bootstrap
      classes depending on the type of loss function.
    * Update the test
    * Add typing

--- kliff/uq/__init__.py
+++ kliff/uq/__init__.py
@@ -1,10 +1,5 @@
 from .mcmc import MCMC, get_T0
 from .mcmc_utils import autocorr, mser, rhat
+from .bootstrap import Bootstrap
 
-__all__ = [
-    "MCMC",
-    "get_T0",
-    "mser",
-    "autocorr",
-    "rhat",
-]
+__all__ = ["MCMC", "get_T0", "mser", "autocorr", "rhat", "Bootstrap"]
