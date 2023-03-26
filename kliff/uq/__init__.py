6d89512d248e61e609802267cbbd0bd7660f1c09
yonatank93 <kurniawanyo@outlook.com>
2023-03-25 22:28:06 -0600

    Also import each bootstrap class for empirical and NN models

--- kliff/uq/__init__.py
+++ kliff/uq/__init__.py
@@ -1,5 +1,14 @@
-from .bootstrap import Bootstrap
+from .bootstrap import Bootstrap, BootstrapEmpiricalModel, BootstrapNeuralNetworkModel
 from .mcmc import MCMC, get_T0
 from .mcmc_utils import autocorr, mser, rhat
 
-__all__ = ["MCMC", "get_T0", "mser", "autocorr", "rhat", "Bootstrap"]
+__all__ = [
+    "MCMC",
+    "get_T0",
+    "mser",
+    "autocorr",
+    "rhat",
+    "Bootstrap",
+    "BootstrapEmpiricalModel",
+    "BootstrapNeuralNetworkModel",
+]
