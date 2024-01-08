a9c0cd89ed02c464eda86041bee278d2831475b4
Amit Gupta <amit_@outlook.in>
2024-01-08 10:56:41 -0600

    Added configuration and property transforms

--- kliff/transforms/property_transforms.py
+++ kliff/transforms/property_transforms.py
@@ -11,16 +11,36 @@ class PropertyTransform:
     A property transform is a function that maps a property of a configuration to a
     transformed property. For example, current property transforms include normalization
     of the energy and forces.
+
+    Attributes:
+        keep_original: If True, the original property values are kept in the configuration
+            object. Otherwise, the original property values are discarded.
+        original_property_value_map: A map that stores the original property values of
+            configurations. It stores list of property values for each configuration.
+        property_key: The key of the property to be transformed.
     """
 
-    def __int__(self):
+    def __int__(self, property_key="energy", keep_original=True):
+        self.keep_original = keep_original
         self.original_property_value_map = None
         self.property_key = "energy"
 
     def transform(self, dataset: Union[List["Configuration"], "Dataset"]):
+        """
+        Transform the property of a configuration.
+
+        Args:
+            dataset: A list of configurations or a dataset.
+        """
         raise PropertyTransformError("This method is not implemented.")
 
     def inverse(self, dataset: Union[List["Configuration"], "Dataset"]):
+        """
+        Inverse transform the property of a configuration.
+
+        Args:
+            dataset: A list of configurations or a dataset.
+        """
         raise PropertyTransformError("This method is not implemented.")
 
     def __call__(self, dataset: Union[List["Configuration"], "Dataset"]):
@@ -30,6 +50,18 @@ class PropertyTransform:
     def get_configuration_list(
         dataset: Union[List["Configuration"], "Dataset"]
     ) -> List["Configuration"]:
+        """
+        Get a list of configurations from a dataset. This method ensures constant API
+        for any arbitrary dataset. It is expected that this method will maintain the
+        order of the Configurations in the dataset. Tihs enqures the inverse property
+        mapping is done correctly.
+
+        Args:
+            dataset: A list of configurations or a dataset.
+
+        Returns:
+            A list of configurations.
+        """
         configuration_list = []
         if isinstance(dataset, Dataset):
             configuration_list = dataset.get_configs()
@@ -41,6 +73,9 @@ class PropertyTransform:
 
 
 class NormalizedPropertyTransform(PropertyTransform):
+    """
+    Normalize the property of a configuration to zero mean and unit variance.
+    """
     def __init__(self, property_key="energy", keep_original=True):
         self.original_property_value_map = None
         self.property_key = property_key
@@ -76,6 +111,10 @@ class NormalizedPropertyTransform(PropertyTransform):
 
 
 class RMSNormalizePropertyTransform(PropertyTransform):
+    """
+    Normalize the property of a configuration to zero mean and unit variance,
+    using the root mean square of the property.
+    """
     def __init__(self, property_key="forces", keep_original=False):
         self.original_property_value_map = None
         self.property_key = property_key
@@ -107,6 +146,11 @@ class RMSNormalizePropertyTransform(PropertyTransform):
 
 
 class RMSMagnitudeNormalizePropertyTransform(PropertyTransform):
+    """
+    Normalize the property of a configuration to zero mean and unit variance,
+    using the root mean square of the magnitude of the property. This method is
+    useful for normalizing forces.
+    """
     def __init__(self, property_key="forces", keep_original=False):
         self.original_property_value_map = None
         self.property_key = property_key
